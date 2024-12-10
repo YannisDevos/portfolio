a520d01607967dd0808d7e6c87c401d68f3f9964
Yannis Devos <yannis.devos.etu@univ-lille.fr>
2024-12-10 23:24:52 +0100

    Almost finished

--- js/script.js
+++ js/script.js
@@ -26,3 +26,32 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+
+// DESC
+
+    //SOQ
+
+function plusDescSlides(n) {
+    showDescSlides1(slideIndex += n);
+}
+
+function currentDescSlide(n) {
+    showDescSlides1(slideIndex = n);
+}
+
+function showDescSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("desc-slide");
+    let dots = document.getElementsByClassName("desc-dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
